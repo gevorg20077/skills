8c94bf8fc07de97fd000875d09e00486e947338c
gevorg20077 <159165957+gevorg20077@users.noreply.github.com>
2024-03-22 21:29:36 +0400

    first commit

---
+++ src/components/Octopus/Octopus.jsx
@@ -0,0 +1,21 @@
+import Skills from '../Skills/Skills'
+import './octopus.css'
+const Octopus = () => {
+  return (
+    <div>
+      <div className="octopus">
+        <Skills />
+        <div className="body"></div>
+        <div className="head">
+          <div class="eyes"></div>
+        </div>
+      </div>
+      <div className="variants">
+        <p className="now">Now</p>
+        <p className="aYearLater">1 year later</p>
+      </div>
+    </div>
+  )
+}
+
+export default Octopus
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,42 @@
+import './skills.css'
+const Skills = () => {
+  return (
+    <div className="skills">
+      <div className="skill skill-HTML">
+        <div className="now skill-HTML-now"><p>HTML</p></div>
+      </div>
+      <div className="skill skill-CSS">
+        <div className="now skill-CSS-now"><p>CSS</p></div>
+      </div>
+      <div className="skill skill-JS">
+        <div className="now skill-JS-now"><p>JavaScript</p></div>
+      </div>
+      <div className="skill skill-React">
+        <div className="now skill-React-now"><p>React.js</p></div>
+      </div>
+      <div className="skill skill-Redux">
+        <div className="now skill-Redux-now"><p>Redux</p></div>
+      </div>
+      <div className="skill skill-Solving">
+        <div className="now skill-Solving-now"><p>Problem solving</p></div>
+      </div>
+      <div className="skill skill-English">
+        <div className="now skill-English-now"><p>English</p></div>
+      </div>
+      <div className="skill skill-Courage">
+        <div className="now skill-Courage-now"><p>Courage</p></div>
+      </div>
+      <div className="skill skill-Active">
+        <div className="now skill-Active-now"><p>Active</p></div>
+      </div>
+      <div className="skill skill-New">
+        <div className="now skill-New-now"><p>New skill</p></div>
+      </div>
+      <div className="skill skill-New">
+        <div className="now skill-New-now"><p>New skill</p></div>
+      </div>
+    </div>
+  )
+}
+
+export default Skills

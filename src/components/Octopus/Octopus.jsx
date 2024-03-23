36abc87c46a769ae45f814b28899b93d00b15c76
gevorg20077 <159165957+gevorg20077@users.noreply.github.com>
2024-03-23 17:28:55 +0400

    first commit

--- src/components/Octopus/Octopus.jsx
+++ src/components/Octopus/Octopus.jsx
@@ -2,21 +2,19 @@ import Skills from '../Skills/Skills'
 import './octopus.css'
 const Octopus = () => {
   return (
-    <div>
-      <div className='container'>
-        <div className="octopus">
-          <Skills />
-          <div className="body"></div>
-          <div className="head">
-            <div class="eyes"></div>
-          </div>
+    <div className='container'>
+      <div className="octopus">
+        <Skills />
+        <div className="body"></div>
+        <div className="head">
+          <div class="eyes"></div>
         </div>
       </div>
       <div className="variants">
-          <p className="now">Now</p>
-          <p className="aYearLater">1 year later</p>
-        </div>
-    </div>
+        <p className="now">Now</p>
+        <p className="aYearLater">1 year later</p>
+      </div>
+    </div >
   )
 }
 

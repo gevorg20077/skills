8864279416b968cf0f525dbb26f16c0cc750bcee
gevorg20077 <159165957+gevorg20077@users.noreply.github.com>
2024-03-27 19:48:35 +0400

    first commit

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -4,39 +4,39 @@ const Skills = () => {
     <div className="skills">
       <dl className="skills__skill skills__HTML">
         <dt className="skills__now skills__HTML_now">HTML</dt>
-        <dd className='skills__percent'>90%</dd>
+        <dd className='skills__percent'>70%</dd>
       </dl>
       <dl className="skills__skill skills__CSS">
         <dt className="skills__now skills__CSS_now">CSS</dt>
-        <dd className='skills__percent'>80%</dd>
+        <dd className='skills__percent'>40%</dd>
       </dl>
       <dl className="skills__skill skills__JS">
         <dt className="skills__now skills__JS_now">JavaScript</dt>
-        <dd className='skills__percent'>86%</dd>
+        <dd className='skills__percent'>45%</dd>
       </dl>
       <dl className="skills__skill skills__React">
         <dt className="skills__now skills__React_now">React.js</dt>
-        <dd className='skills__percent'>88%</dd>
+        <dd className='skills__percent'>58%</dd>
       </dl>
       <dl className="skills__skill skills__Redux">
         <dt className="skills__now skills__Redux_now">Redux</dt>
-        <dd className='skills__percent'>90%</dd>
+        <dd className='skills__percent'>70%</dd>
       </dl>
       <dl className="skills__skill skills__Solving">
         <dt className="skills__now skills__Solving_now">Problem solving</dt>
-        <dd className='skills__percent'>78%</dd>
+        <dd className='skills__percent'>37%</dd>
       </dl>
       <dl className="skills__skill skills__English">
         <dt className="skills__now skills__English_now">English</dt>
-        <dd className='skills__percent'>70%</dd>
+        <dd className='skills__percent'>30%</dd>
       </dl>
       <dl className="skills__skill skills__Courage">
         <dt className="skills__now skills__Courage_now">Courage</dt>
-        <dd className='skills__percent'>80%</dd>
+        <dd className='skills__percent'>42%</dd>
       </dl>
       <dl className="skills__skill skills__Active">
         <dt className="skills__now skills__Active_now">Active</dt>
-        <dd className='skills__percent'>90%</dd>
+        <dd className='skills__percent'>50%</dd>
       </dl>
       <dl className="skills__skill skills__New">
         <dt className="skills__now skills__New_now">New skill</dt>
